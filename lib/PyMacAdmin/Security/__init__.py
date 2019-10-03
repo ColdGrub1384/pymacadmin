eb9cccfdb688576a0b68af20e98ab0b6468ab240
Adrian Labbé <adrilabbelol@gmail.com>
2019-10-03 15:36:38 -0300

    Update Security.framework path

--- lib/PyMacAdmin/Security/__init__.py
+++ lib/PyMacAdmin/Security/__init__.py
@@ -7,7 +7,7 @@ import sys
 # This is not particularly elegant but to avoid everything having to load the
 # Security framework we use a single copy hanging of this module so everything
 # else can simply use Security.lib.SecKeychainFoo(…)
-lib = PyMacAdmin.load_carbon_framework('/System/Library/Frameworks/Security.framework/Versions/Current/Security')
+lib = PyMacAdmin.load_carbon_framework('/System/Library/Frameworks/Security.framework/Security')
 
 CSSM_DB_RECORDTYPE_APP_DEFINED_START = 0x80000000
 CSSM_DL_DB_RECORD_X509_CERTIFICATE   = CSSM_DB_RECORDTYPE_APP_DEFINED_START + 0x1000
